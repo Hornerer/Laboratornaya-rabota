1fd219be9d928ffec7f9783a9898901518483934
Hornerer <74593778+Hornerer@users.noreply.github.com>
2020-11-25 17:47:00 +0300

    Add files via upload

---
+++ Zadanie 1.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <cstring>
+bool strobogrammatic_number(std::string num) {
+	for (int i = 0; i < num.length() / 2; ++i) {
+		if (num[i] == '6') {
+			if (num[num.length() - 1 - i] != '9') {
+				return false;
+
+			}
+		}
+		else if (num[i] == '9') {
+			if (num[num.length() - 1 - i] != '6') {
+				return false;
+			}
+		}
+		else if (num[num.length()-i-1] !=num[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	std::string num;
+	std::cin >> num;
+	if (strobogrammatic_number(num)) {
+		std::cout << "True";
+	}
+	else {
+		std::cout<<"False";
+	}
+	return 0;
+	
+}
+
+
---
+++ Zadanie 2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int n, x=0;
+	std::cin >> n;
+	while (n>0) {
+		if (n % 4 > 0) {
+			break;
+		}
+		else {
+			n = n / 4;
+			++x;
+
+		}
+	}
+	if (x > 0) {
+		std::cout << "True";
+	}
+	else {
+		std::cout << "False";
+	}
+}
+
+
